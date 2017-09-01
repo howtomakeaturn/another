06484e176feb928bf6c4d4797e517e8224eb0167
chuan-hao, you <howtomakeaturn@hotmail.com>
2017-09-01 14:09:14 +0800

    design the project page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,6 +22,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
         @yield('head')
 
     </head>
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -2,10 +2,16 @@
 
 @section('head')
 
+
+
 <style>
     body {
         background-color: #EEEEEE   ;
         font-size: 16px;
+    }
+    .main-title {
+        margin-bottom: 20px;
+        padding-left: 15px;
     }
     .issues-box {
         background-color: white;
@@ -17,25 +23,39 @@
     .issue-box.-closed {
         background-color: #F5F5F5;
     }
-
     ._size-20 {
         font-size: 20px;
+    }
+    .-subinfo {
+        color: #9e9e9e;
+        font-size: 14px;
+    }
+    ._margin-bottom-5 {
+        margin-bottom: 5px;
     }
 </style>
 
 @endsection
 
 @section('content')
-    <br>
     <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="main-title">Just Another Demo Project</h2>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-8">
                 <div class="issues-box">
                     @for($i=0; $i<5; $i++)
                     <div class="issue-box">
                         <div class="row">
-                            <div class="col-md-10">
-                                101010
+                            <div class="col-md-9">
+                                <div class="_margin-bottom-5">this is just another issue #{{$i}}</div>
+                                <div class="-subinfo">{{$i}} 天前</div>
+                            </div>
+                            <div class="col-md-1">
+                                <i class="fa fa-comment-o" aria-hidden="true"></i> {{$i}}
                             </div>
                             <div class="col-md-2">
                                 <span class="label label-success _size-20">Open</span>
@@ -46,8 +66,12 @@
                     @for($i=0; $i<5; $i++)
                     <div class="issue-box -closed">
                         <div class="row">
-                            <div class="col-md-10">
-                                101010
+                            <div class="col-md-9">
+                                <div class="_margin-bottom-5">this is just another issue #{{$i}}</div>
+                                <div class="-subinfo">{{$i}} 天前</div>
+                            </div>
+                            <div class="col-md-1">
+                                <i class="fa fa-comment-o" aria-hidden="true"></i> {{$i}}
                             </div>
                             <div class="col-md-2">
                                 <span class="label label-default _size-20">Closed</span>
@@ -58,8 +82,10 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <a class="btn btn-success btn-block" href='#'>New Issue</a>
+                <a class="btn btn-success btn-block _size-20" href='#'>New Issue</a>
             </div>
         </div>
     </div>
+    <br>
+    <br>
 @endsection
